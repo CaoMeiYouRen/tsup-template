8490bec2576901505ae1772b820d4ea48c653f06
CaoMeiYouRen <996881204@qq.com>
2024-07-18 00:50:32 +0800

    fix: 修复 esbuild 构建时的编码格式

--- tsup.config.ts
+++ tsup.config.ts
@@ -34,7 +34,10 @@ export default defineConfig({
     dts: true,
     minify: false, // 缩小输出
     shims: true, // 注入 cjs 和 esm 填充代码，解决 import.meta.url 和 __dirname 的兼容问题
-    // external: [],
+    esbuildOptions(options, context) { // 设置编码格式
+        options.charset = 'utf8'
+    },
+    // external: [], // 排除的依赖项
     // noExternal: [/(.*)/], // 将依赖打包到一个文件中
     // bundle: true,
 })
